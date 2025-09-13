68c2dc9b3a3d9111e7f817132e6523d7a47a9692
dumplys <57139609+bimbap@users.noreply.github.com>
2025-09-13 11:46:24 +0700

    add code bookget.py

--- lambda/bookget.py
+++ lambda/bookget.py
@@ -1,26 +1,30 @@
 import json
 import boto3
 
-# Initialising the DynamoDB client
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('books')  # The name of your DynamoDB table
+table = dynamodb.Table('books')
 
 def lambda_handler(event, context):
     try:
-        params = {
-            'TableName': 'books'
-        }
-        
-        # Utilising the scan method to get all items in the table
-        response = table.scan(**params)
-        
-        data = response['Items']
-        
+        response = table.scan()
+        data = response.get('Items', [])
+
         return {
             'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Headers': 'Content-Type,Authorization',
+                'Access-Control-Allow-Methods': 'GET,POST,OPTIONS'
+            },
             'body': json.dumps(data)
         }
     except Exception as e:
+        # log error supaya ketahuan di CloudWatch
+        print("Error:", str(e))
         return {
-            'statusCode': 500
+            'statusCode': 500,
+            'headers': {
+                'Access-Control-Allow-Origin': '*'
+            },
+            'body': json.dumps({'message': 'Internal Server Error'})
         }
